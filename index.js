190e2cc8cc401dcd7e4df0f1d3ab6ce10713df2d
Shadow <william.shadow@outlook.com>
2021-08-18 14:45:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config()
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
+client.on("ready", () => console.log("Connected"))
+
+
 client.on("guildMemberAdd", (member) => {
     if(member.guild.id != "836386335840665610") return
     let m = client.channels.cache.get("841877170930450452").send(`Welcome, ${member}, to SudoSecurity's Community Discord Server! We are a friendly and laid back community of people who love and hobby around technology from hardware to Linux. We also play games with each other like Minecraft or Valorant. We hope your enjoy your stay!`)
