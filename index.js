a3129525ff2fc10d55cf06dace7c7969a21c361b
Shadow <william.shadow@outlook.com>
2021-09-21 15:11:03 -0500

    New #chat channel

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.on("ready", () => console.log("Connected"))
 
 client.on("guildMemberAdd", (member) => {
     if(member.guild.id != "836386335840665610") return
-    let m = client.channels.cache.get("841877170930450452").send(`Welcome, ${member}, to SudoSecurity's Community Discord Server! We are a friendly and laid back community of people who love and hobby around technology from hardware to Linux. We also play games with each other like Minecraft or Valorant. We hope your enjoy your stay!`)
+    let m = client.channels.cache.get("889950570520145991").send(`Welcome, ${member}, to SudoSecurity's Community Discord Server! We are a friendly and laid back community of people who love and hobby around technology from hardware to Linux. We also play games with each other like Minecraft or Valorant. We hope your enjoy your stay!`)
 })
 
 client.on("message", (message) => {
